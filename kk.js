319b7c5c029a29ce3b861bfa603c98cbadca530d
MohdKhalid13 <100787727+MohdKhalid13@users.noreply.github.com>
2022-08-04 01:53:57 +0530

    Add files via upload

---
+++ kk.js
@@ -0,0 +1,72 @@
+$(document).ready(function()
+{
+  $("#head").click(function()
+  {
+    $("#body").slideToggle("fast");
+  });
+});
+$(document).ready(function()
+{
+  $("#head1").click(function()
+  {
+    $("#body1").slideToggle("fast");
+  });
+});
+$(document).ready(function()
+{
+  $("#head2").click(function()
+  {
+    $("#body2").slideToggle("fast");
+  });
+});
+$(document).ready(function()
+{
+  $("#head3").click(function()
+  {
+    $("#body3").slideToggle("fast");
+  });
+});
+$(document).ready(function()
+{
+  $("#head4").click(function()
+  {
+    $("#body4").slideToggle("fast");
+  });
+});
+
+$(document).ready(function()
+{
+  $("#head5").click(function()
+  {
+    $("#body5").slideToggle("fast");
+  });
+});
+function instagram(){
+    window.open("https://www.instagram.com/md.khalid_13");
+}
+function youtube(){
+  window.open("https://youtube.com/channel/UCtcvk8oC-0QyhesM5Gske_A");
+}
+function linkedin(){
+  window.open("https://www.linkedin.com/in/mohammad-khalid-ajaj-ahmed-mansuri-b910b9187")
+}
+function Github(){
+  window.open("https://github.com/MohdKhalid13")
+}
+function fb(){
+  window.open("https://www.facebook.com/profile.php?id=100011265421244")
+}
+function twitter(){
+  window.open("https://twitter.com/railway166?t=P1dJAKhiHLiKRJ9glzjvvQ&s=08")
+}
+function start(){
+  location.reload();
+}
+function Donate(){
+  window.open("./MK_QR_Code.jpeg")
+}
+function mail(){
+  window.open("mailto:cadcad166@gmail.com");
+}
+
+
